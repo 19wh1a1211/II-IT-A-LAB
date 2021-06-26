2f9f5f7c87f84a195ffb6b10da12d3c6e6920208
19wh1a1211 <69144859+19wh1a1211@users.noreply.github.com>
2021-06-26 16:45:12 +0530

    Add files via upload

---
+++ JAVA LAB/CYCLE-1/BASIC PROGRAMS/add.java
@@ -0,0 +1,11 @@
+class add {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Enter two numbers");
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    System.out.println("The sum is: " + sum);
+  }
+}
