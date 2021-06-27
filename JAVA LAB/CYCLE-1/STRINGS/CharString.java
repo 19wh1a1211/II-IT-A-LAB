6d0cd7001c8fefbb10ed07aac1b39c4bebf1ea6a
19wh1a1211 <69144859+19wh1a1211@users.noreply.github.com>
2021-06-27 13:02:20 +0530

    Add files via upload

---
+++ JAVA LAB/CYCLE-1/STRINGS/CharString.java
@@ -0,0 +1,8 @@
+public class CharString {
+
+    public static void main(String[] args) {
+        char ch = 's';
+        String st = Character.toString(ch);
+        System.out.println("The string is: " + st);
+    }
+}
